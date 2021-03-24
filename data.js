30bbe7510bc68ab3fc972da8e68f7c6fe668b9de
alabi noimot <kikkyal@gmail.com>
2021-03-24 04:10:54 +0100

    initial commit to E-menu repo

---
+++ data.js
@@ -0,0 +1,202 @@
+ Data = [
+    {
+        id: 1,
+        city: 'Lagos',
+        restaurant: 'Cafe One by Sterling',
+         Image: 'https://tse4.mm.bing.net/th?id=OIP.Ks4ncrGL5Q2L5GPnWDOAWAHaE8&pid=Api&P=0&w=249&h=167',
+        Address: 'Lennox Mall, Plot 2/3 Block A10 Admiralty Way, Lekki Phase 1, Lagos',
+        contact: 09053872953,
+        Menu: ['Coffee & Tea', 'Belgian', 'smoothies', 'sandwiches'],
+        keyword: ['Lagos', 'Coffee & Tea', 'Smoothies', 'sandwiches']
+    },
+    {
+        id: 2,
+        city: 'Lagos',
+        restaurant: 'Kravings Diner',
+        Image: 'https://tse4.mm.bing.net/th?id=OIP.XfljTpQbZ3HfRGvtu5rCuAHaDt&pid=Api&P=0&w=312&h=156',
+        Address: '21A Kofo Abayomi street, Victoria Island, Lagos',
+        contact: 07064327235,
+        Menu: ['American', 'European']
+    },
+    {
+        id: 3,
+        city: 'Lagos',
+        restaurant: 'Sweets & Pasrties',
+        Image: 'https://s3-media2.fl.yelpcdn.com/bphoto/LpjOjK1mbxCAC9nxdp5YOA/ls.jpg',
+        Address: 'Maryland Mall, 350-360 Ikorodu Road, Maryland, Lagos',
+        contact: 08028339228,
+        Menu: ['Bakery', 'Dessert', 'Ice Cream', 'Pasries']
+    },
+    {
+        id: 4,
+        city: 'Lagos',
+        restaurant: 'TFS',
+        Image: 'https://tse4.mm.bing.net/th?id=OIP.IQu0JIcKRTCSPT3iPvTThgAAAA&pid=Api&P=0&w=300&h=185',
+        Address: '61 Adeniran Ogunsanya Street, Surulere, Lagos',
+        contact: 08075491145,
+        Menu: ['Fast Food', 'Shawarma', 'Nigerian']
+    },
+    {
+        id: 5,
+        city: 'Lagos',
+        restaurant: 'Burgers & Shakes',
+        Image: 'https://tse2.mm.bing.net/th?id=OIP.XQEvAeTvbGtvmnVzo8XQNwHaE8&pid=Api&P=0&w=265&h=178',
+        Address: '92 Adniran Ogunsanya, Opp. Sky Bank, Surulere, Lagos',
+        contact: 09050505067,
+        Menu: ['American', 'Burgers', 'Juice & Smoothies']
+    },
+    {
+        id: 6,
+        city: 'Lagos',
+        restaurant: 'Payless Afrique De Kitchen',
+        Image: 'https://tse4.mm.bing.net/th?id=OIP.fE06WuGuiYi9av1-G7r5bwHaEK&pid=Api&P=0&w=311&h=176',
+        Address: 'Surulere Leisure Mall, 97-99 Adeniran Ogunsanya street, Surulere, Lagos',
+        contact: 08052428284,
+        Menu: ['Nigerian']
+    },
+    {
+        id: 7,
+        city: 'Lagos',
+        restaurant: 'Yellow Chilli',
+        Image: 'https://i.pinimg.com/originals/cb/96/6b/cb966b44a044bc5b63472787dd8e64e0.jpg',
+        Address: '35 Joel Ogunnaike Street, Ikeja, Lagos',
+        contact: 08089948999,
+        Menu: ['Nigerian', 'African', 'Calabar & Efik']
+    },
+    {
+        id: 8,
+        city: 'Lagos',
+        restaurant: 'The Place',
+        Image: 'https://tse4.mm.bing.net/th?id=OIP.0_UD0-jR2nedAwKft5wsggHaFj&pid=Api&P=0&w=236&h=178',
+        Address: 'Circle Mall, Lekki-Epe Expressway, 5th (Jakande) Roundabout, Lekki, Lagos',
+        contact: 08084467727,
+        Menu: ['Nigerian']
+    },
+    {
+        id: 9,
+        city: 'Lagos',
+        restaurant: 'Yogurt Frenzy',
+        Image: 'https://d8ys5mrbqhmjx.cloudfront.net/nigeria/companies/yogurt-frenzy/large/yogurt-frenzy-138105.jpg',
+        Address: '77 Ago Palace Way, Okota Road, Oshodi-Isolo, Lagos',
+        contact: 08038797393,
+        Menu: ['Froyo', 'Ice Cream', 'Yoghurt']
+    },
+    {
+        id: 10,
+        city: 'Lagos',
+        restaurant: 'Pizza Lounge',
+        Image: 'https://tse3.mm.bing.net/th?id=OIP.v4dqfXtAtfqrc_Zxus72pQHaJ4&pid=Api&P=0&w=300&h=300',
+        Address: 'Lateef Salami Street , Ajao Estate, Oshodi-Isolo, Lagos',
+        contact: 09023992119,
+        Menu: ['Fast Food', 'Pizza', 'Sandwiches']
+    },
+    {
+        id: 11,
+        city: 'Lagos',
+        restaurant: 'Lasgidi Buka',
+        Image: 'https://scontent-los2-1.xx.fbcdn.net/v/t1.0-0/s640x640/164072708_1755270934641390_7886835692194644053_o.jpg?_nc_cat=101&ccb=1-3&_nc_sid=730e14&_nc_eui2=AeF1zxl5-tgWQkLmnYCEmQPyLlLO-db52ZguUs751vnZmJqeXSFEYcW6vUXB9G4qZ1PCITSLGVNc-HiEk7VxRoZk&_nc_ohc=FPe9tp-aUQQAX8458k3&_nc_ht=scontent-los2-1.xx&tp=7&oh=c75acd4d570b1ed0a303bcc54570f59a&oe=6080E9C8',
+        Address: '21 Admiralty road, Beside Daytona, Lekki Phase 1, Lagos',
+        contact: 08092927825,
+        Menu: ['African', 'Calabar & Efik', 'Nigerian', 'Pepper Soup', 'Seafood']
+    },
+    {
+        id: 12,
+        city: 'Abuja',
+        restaurant: 'Street Cafe',
+        Image: 'https://tse2.mm.bing.net/th?id=OIP.KZUTwF3-Zw1dIJxo67Ur4QHaFj&pid=Api&P=0&w=206&h=155',
+        Address: '41 Adetokunbo Ademola street, Opp H Medix, Wuse 2, Abuja',
+        contact: 08112616664,
+        Menu: ['Fast Food', 'Burger', 'Sandwiches']
+    },
+    {
+        id: 13,
+        city: 'Abuja',
+        restaurant: 'Signature',
+        Image: 'https://files.ofadaa.com/uploads/restaurant_photo/file/852/normal_signature-restaurant-abuja-01.jpg',
+        Address: '38 Lingu Crescent, Off Aminu Kano Crescent, Wuse 2, Abuja',
+        contact: 08180003502,
+        Menu: ['Inter-Continental']
+    },
+    {
+        id: 14,
+        city: 'Abuja',
+        restaurant: 'Grills In & Out',
+        Image: 'https://tse4.mm.bing.net/th?id=OIP.QXTmBt7xd8zwRxPrN1M4mQHaEs&pid=Api&P=0&w=280&h=179',
+        Address: 'Food City, 1 Kindia Close, Bangui street, Wuse 2, Abuja',
+        contact: 08073168682,
+        Menu: ['Barbecue', 'Grills', 'Korean', 'Nigerian']
+    },
+    {
+        id: 15,
+        city: 'Abuja',
+        restaurant: 'Sweet Treats',
+        Image: 'https://files.ofadaa.com/uploads/restaurant_photo/file/1319/normal_Sweet-Treats-Abuja-01.jpg',
+        Address: '1031 Obafemi Awolowo way, Behind Eterna Filling Station, Utako, Abuja',
+        contact: 09091214052,
+        Menu: ['Bakery', 'Dessert', 'Froyo', 'Pastries', 'Salads', 'Sandwiches']
+    },
+    {
+        id: 16,
+        city: 'Abuja',
+        restaurant: 'Uptown',
+        Image: 'https://tse3.mm.bing.net/th?id=OIP.SsMYdMwRiTSINO4RnaRndgHaFc&pid=Api&P=0&w=215&h=159',
+        Address: '176 Aminu Kano Crescent, Wuse 2, Abuja',
+        contact: 08167859315,
+        Menu: ['Asian']
+    },
+    {
+        id: 17,
+        city: 'Abuja',
+        restaurant: 'KFC',
+        Image: 'https://tse4.mm.bing.net/th?id=OIP.GwYGA34dG2rOxo_KMEmzaAHaEK&pid=Api&P=0&w=306&h=173',
+        Address: 'Grand Tower mall, Gudu, Abuja',
+        contact: 08123672888,
+        Menu: ['American', 'Burgers', 'Fast Food']
+    },
+    {
+        id: 18,
+        city: 'Abuja',
+        restaurant: 'Bon Cafe',
+        Image: 'https://scontent-yyz1-1.xx.fbcdn.net/v/t1.0-9/s720x720/64308764_2360783497335747_4594631145676603392_o.jpg?_nc_cat=111&ccb=3&_nc_sid=110474&_nc_ohc=_FseSwFTT9kAX_mXrrE&_nc_ht=scontent-yyz1-1.xx&tp=7&oh=520872a9483851fb29c610ae133d37df&oe=605EB7BA',
+        Address: 'Ceddi Plaza, 264 Tafawa Balewa way, CBD, Abuja',
+        contact: 08191782278,
+        Menu: ['BakeryBurgersCoffee & TeaDessert', 'Fast Food', 'Ice Cream', 'Pastries', 'Pizza', 'Sandwiches', 'Shawarma']
+    },
+    {
+        id: 19,
+        city: 'Abuja',
+        restaurant: 'Waffle Stop Café',
+        Image: 'https://files.ofadaa.com/uploads/restaurant_photo/file/2612/normal_waffle-stop-cafe-abuja-01.jpg',
+        Address: 'Dunes Centre, 44 Aguiyi Ironsi Way, Maitama, Abuja',
+        contact: 09034633092,
+        Menu: ['Pancake', 'Waffle']
+    },
+    {
+        id: 20,
+        city: 'Abuja',
+        restaurant: 'GoPro Restaurant',
+        Image: 'https://tse4.mm.bing.net/th?id=OIP.JPGy65_jUDVhi0-cDLc83QHaE8&pid=Api&P=0&w=235&h=158',
+        Address: '67 Abidjan street , Wuse 1, Abuja',
+        contact: 08160000180,
+        Menu: ['Calabar & Efik', 'Nigerian', 'Seafood']
+    },
+    {
+        id: 21,
+        city: 'Abuja',
+        restaurant: 'Iya Oyo Amala',
+        Image: 'https://tse1.mm.bing.net/th?id=OIP.peM3GcvXmdr57VPxpMswXQHaE6&pid=Api&P=0&w=232&h=155',
+        Address: '112 Idris Gidado street, Wuye, Abuja',
+        contact: 08039662370,
+        Menu: ['Nigerian']
+    },
+    {
+        id: 22,
+        city: 'Abuja',
+        restaurant: 'Munch On Wheels',
+        Image: 'https://tse2.mm.bing.net/th?id=OIP.KZUTwF3-Zw1dIJxo67Ur4QHaFj&pid=Api&P=0&w=206&h=155',
+        Address: 'Federal Secretariat / Opp H-Medix , Wuse 2, Abuja',
+        contact: 09098636869,
+        Menu: ['Barbecue', 'Burgers', 'Fast Food', 'Shawarma']
+    },
+
+]
---
+++ index.js
@@ -0,0 +1,62 @@
+
+const restaurantData = document.querySelector('.restaurant-data');
+const lagosBtn = document.querySelector('#lagosBtn');
+const allCitiesBtn = document.querySelector('#allCitiesBtn')
+
+
+window.addEventListener('load', loadData())
+
+function loadData() {
+    Data.forEach((data) => {
+
+        let container = document.createElement('div')
+        container.classList.add("data-container")
+        container.innerHTML = ` 
+        <div class="menu-image">
+                <img src="${data.Image}" alt="${data.restaurant}" />
+            </div>
+            <div class="restaurant-info">
+                <h3>Restaurant: ${data.restaurant}</h3>
+                <p>Addrress: ${data.Address}</P>
+                <p>Menu: ${data.Menu}</P>
+                <div>
+                    <small>City: ${data.city}</small>
+                    <small>Phone Number: ${data.contact}</small>
+                </div>
+
+            </div>`
+        restaurantData.append(container)
+    })
+}
+
+
+allCitiesBtn.addEventListener('click', () => loadData())
+
+lagosBtn.addEventListener('click', () => searchCity("Lagos"))
+
+function searchCity(city) {
+    console.log(city)
+    let searchResult = Data.filter((item) => item.city===city)
+    if(searchResult.length===0) return alert(`${city} not found`);
+    restaurantData.innerHTML=``
+    searchResult.forEach((data) => {
+
+        let container = document.createElement('div')
+        container.classList.add("data-container")
+        container.innerHTML = ` 
+        <div class="menu-image">
+                <img src="${data.Image}" alt="${data.restaurant}" />
+            </div>
+            <div class="restaurant-info">
+                <h3>Restaurant: ${data.restaurant}</h3>
+                <p>Addrress: ${data.Address}</P>
+                <p>Menu: ${data.Menu}</P>
+                <div class="restaurant-contact">
+                    <small>City: ${data.city}</small>
+                    <small>Phone Number: ${data.contact}</small>
+                </div>
+
+            </div>`
+        restaurantData.append(container)
+    })
+}
